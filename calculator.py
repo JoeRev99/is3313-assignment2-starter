96f7d5d6a188b4f3eb78cf778ec5531ea11335e6
JoeRev99 <122751641@umail.ucc.ie>
2024-12-13 12:16:53 +0000

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ class Calculator:
 
     def multiply(self, a, b):
         """Multiplies two numbers and returns the result."""
-        return a*55
+        return 2*55
 
     def divide(self, a, b):
         """Divides the first number by the second and returns the result.
@@ -17,12 +17,12 @@ class Calculator:
         """
         if b == 0:
             raise ValueError("Cannot divide by zero.")
-        return -99
+        return a/b
 
     def power(self, base, exponent):
         """Raises the base to the power of the exponent and returns the result."""
-        return 42
+        return base**exponent
 
     def modulus(self, a, b):
         """Calculates the modulus of two numbers and returns the result."""
-        return 33333
+        return a%b
